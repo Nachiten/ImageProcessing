4d4e15e6a22f3a3810433cbc883b822ed3e609d2
Ignacio Baptista [PC] <ibapista@frba.utn.edu.ar>
2021-04-09 21:42:34 -0300

    Agregados mas colores tolerados

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy
 
 
+
 def printearColor(color):
     if color == 'R':
         printearMatriz(0)
@@ -30,25 +31,43 @@ def printearMatriz(elemento):
         print("")
 
 
-def modificarMatrizEnIndex(index, limite):
+def modificarMatrizEnIndex(indexFuncion, limite):
     for unaFila in range(0, height):
         for unaColumna in range(0, width):
-
-            # 0 = r
-            # 1 = g
-            # 2 = b
-            # 3 = y
-
-            if index == 3:
+            if indexFuncion == 3:  # YELLOW
                 if numpy_array[unaFila][unaColumna][0] >= limite \
-                        and numpy_array[unaFila][unaColumna][1] >= limite\
+                        and numpy_array[unaFila][unaColumna][1] >= limite \
                         and numpy_array[unaFila][unaColumna][2] <= 50:
+                    nuevaMatriz[unaFila][unaColumna] = 1
+
+            elif indexFuncion == 4:  # PINK
+                if numpy_array[unaFila][unaColumna][0] >= limite \
+                        and numpy_array[unaFila][unaColumna][1] <= 50 \
+                        and numpy_array[unaFila][unaColumna][2] >= limite:
+                    nuevaMatriz[unaFila][unaColumna] = 1
+
+            elif indexFuncion == 5:  # CYAN
+                if numpy_array[unaFila][unaColumna][0] <= 50 \
+                        and numpy_array[unaFila][unaColumna][1] >= limite \
+                        and numpy_array[unaFila][unaColumna][2] >= limite:
+                    nuevaMatriz[unaFila][unaColumna] = 1
+
+            elif indexFuncion == 6:  # WHITE
+                if numpy_array[unaFila][unaColumna][0] >= 250 \
+                        and numpy_array[unaFila][unaColumna][1] >= 250 \
+                        and numpy_array[unaFila][unaColumna][2] >= 250:
+                    nuevaMatriz[unaFila][unaColumna] = 1
+
+            elif indexFuncion == 7:  # BLACK
+                if numpy_array[unaFila][unaColumna][0] <= 7 \
+                        and numpy_array[unaFila][unaColumna][1] <= 7 \
+                        and numpy_array[unaFila][unaColumna][2] <= 7:
                     nuevaMatriz[unaFila][unaColumna] = 1
             else:
                 valoresNoPosibles = [0, 1, 2]
-                valoresNoPosibles.remove(index)
+                valoresNoPosibles.remove(indexFuncion)
 
-                if numpy_array[unaFila][unaColumna][index] >= limite \
+                if numpy_array[unaFila][unaColumna][indexFuncion] >= limite \
                         and numpy_array[unaFila][unaColumna][valoresNoPosibles[0]] <= 50 \
                         and numpy_array[unaFila][unaColumna][valoresNoPosibles[1]] <= 50:
                     nuevaMatriz[unaFila][unaColumna] = 1
@@ -56,18 +75,29 @@ def modificarMatrizEnIndex(index, limite):
                     nuevaMatriz[unaFila][unaColumna] = 0
 
 
-def transformarMatriz(colorActual, limite):
-    if colorActual == 'R':
-        modificarMatrizEnIndex(0, limite)
+def transformarMatriz(indexFuncion, limite):
+    if indexFuncion == 0:
+        colorFuncion = 'R'
+    elif indexFuncion == 1:
+        colorFuncion = 'G'
+    elif indexFuncion == 2:
+        colorFuncion = 'BLU'
+    elif indexFuncion == 3:
+        colorFuncion = 'Y'
+    elif indexFuncion == 4:
+        colorFuncion = 'P'
+    elif indexFuncion == 5:
+        colorFuncion = 'C'
+    elif indexFuncion == 6:
+        colorFuncion = 'W'
+    elif indexFuncion == 7:
+        colorFuncion = 'BLA'
+    else:
+        colorFuncion = 'R'
 
-    if colorActual == 'G':
-        modificarMatrizEnIndex(1, limite)
+    modificarMatrizEnIndex(index, limite)
 
-    if colorActual == 'B':
-        modificarMatrizEnIndex(2, limite)
-
-    if colorActual == 'Y':
-        modificarMatrizEnIndex(3, limite)
+    return colorFuncion
 
 
 def vecinosDe(fila, columna):
@@ -122,22 +152,11 @@ def contarClusters():
 image = Image.open('D:\Escritorio\ImagenPrueba.PNG')
 width, height = image.size
 
-for index in range(0, 4):
+for index in range(0, 8):
     numpy_array = numpy.array(image)
     nuevaMatriz = [[0 for _ in range(width)] for _ in range(height)]
 
-    if index == 0:
-        color = 'R'
-    elif index == 1:
-        color = 'G'
-    elif index == 2:
-        color = 'B'
-    elif index == 3:
-        color = 'Y'
-    else:
-        color = 'R'
-
-    transformarMatriz(color, 200)
+    color = transformarMatriz(index, 200)
 
     while hayAlgunCluster():
         pass
@@ -146,13 +165,15 @@ for index in range(0, 4):
 
     print("Hay una cantidad de: " + str(cantidadClusters) + " clusters de color: " + color + " en la imagen.")
 
-
 # Answers
-# Amarillo: 9
-# Rojo: 3
-# Cyan: 3
-# Verde: 1
-# Rosa: 4
-# Azul: 12
+
 # Blanco:	4
 # Negro:	4
+
+# Rojo: 3 R
+# Verde: 1 G
+# Azul: 12 B
+
+# Amarillo: 9 Y
+# Cyan: 4 C
+# Rosa: 4 P
